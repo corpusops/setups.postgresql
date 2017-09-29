30fff4e5b1ec0400d890480a3b76cccbf4d40398
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2017-09-29 23:28:38 +0200

    fix 9.2 build

--- .docker/provision.sh
+++ .docker/provision.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -ex
-rsync $RSYNC_OPTS "$_P.in/" "$_P/" --exclude=local --delete-excluded
-export ANSIBLE_FORCE_COLOR=1 PYTHONUNBUFFERED=1
 log() { echo "${@}" >&2; }
 vv() { log "$@"; "$@"; }
+rsync $RSYNC_OPTS "$_P.in/" "$_P/" --exclude=local --delete-excluded
+export ANSIBLE_FORCE_COLOR=1 PYTHONUNBUFFERED=1
+lcops="$_P.in/local/corpusops.bootstrap/"
+
 do_install() {
     cd "$_P"
     $ANSIBLE_PLAYBOOK \
@@ -17,6 +19,20 @@ do_up() {
     cd "$COPS_ROOT"
     bin/install.sh -C -s
 }
+use_local_cops() {
+    set +x
+    if [ -e "$lcops" ];then
+        log "Local checkout of corpusops.bootstrap; using it"
+        for i in \
+            bin/ doc/ docker/ hacking/ LICENSE.txt \
+            playbooks/ README.md requirements/ roles/ setup.py src/ \
+            ;do
+            rsync -a "$lcops/$i" "$COPS_ROOT/$i"
+        done
+    fi
+    set -x
+}
+use_local_cops
 if [[ -n ${DO_UP-} ]] && ! do_up;then
     log "do re-upgrade failed"
     exit 3
