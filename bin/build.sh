95e4cff0b77e560d2a329edf258d71440a593fe7
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2018-01-08 01:47:30 +0100

    Refresh livepacker test script

--- bin/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-OW=$(pwd)
-W=$(cd "$(dirname $0)/.." && pwd)
-COPS_ROOT="$W/local/corpusops.bootstrap"
-"$COPS_ROOT/hacking/docker_build_chain.py" $@
-# vim:set et sts=4 ts=4 tw=80:
